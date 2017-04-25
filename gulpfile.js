104038b27b6e6ea000598f135e64a19eebf61672
Thom <thom@imacs-iMac.local>
2017-04-25 15:35:42 +0100

    tidied leading whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -26,40 +26,40 @@ let imagemin = require('gulp-imagemin')
 
 // Define I/O paths
 let path = {
-    css: {
-        i: './src/scss/**/*.scss',
-        o: './dist/css/'
-    },
-    html: {
-        i: './src/**/*.html',
-        o: './dist/'
-    },
-    js: {
-        i: './src/js/**/*.js',
-        o: './dist/js'
-    },
-    img: {
-        i: './src/img/**/*',
-        o: './dist/img'
-    },
-    data: {
-        i: './src/data/**/*.json',
-        o: './dist/data'
-    },
-    include: {
-      i: './src/include/**/*',
-      o: './dist/include'
-    }
+	css: {
+		i: './src/scss/**/*.scss',
+		o: './dist/css/'
+	},
+	html: {
+		i: './src/**/*.html',
+		o: './dist/'
+	},
+	js: {
+		i: './src/js/**/*.js',
+		o: './dist/js'
+	},
+	img: {
+		i: './src/img/**/*',
+		o: './dist/img'
+	},
+	data: {
+		i: './src/data/**/*.json',
+		o: './dist/data'
+	},
+	include: {
+		i: './src/include/**/*',
+		o: './dist/include'
+	}
 }
 
 // Define options
 let sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'expanded'
+	errLogToConsole: true,
+	outputStyle: 'expanded'
 }
 
 let autoprefixerOptions = {
-    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+	browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
 }
 
 let envProd = false
@@ -67,92 +67,101 @@ let envProd = false
 // TASKS
 
 gulp.task('default', function(callback) {
-    sassOptions = {
-        errLogToConsole: true,
-        outputStyle: 'expanded'
-    }
-    envProd = false
-    runSequence('clean:dist', 'sass', 'html', 'js', 'img', 'data', 'include', callback)
+	sassOptions = {
+		errLogToConsole: true,
+		outputStyle: 'expanded'
+	}
+	envProd = false
+	runSequence('clean:dist', 'sass', 'html', 'js', 'img', 'data', 'include', callback)
 })
 
 // Watching for changes
 gulp.task('watch', function(callback) {
-    runSequence('clean:dist', 'default', function() {
-        browserSync.init({server: "dist"})
-        gulp.watch(path.js.i, ['js', browserSync.reload])
-        gulp.watch(path.css.i, ['sass', browserSync.reload])
-        gulp.watch(path.html.i, ['html', browserSync.reload])
-        gulp.watch(path.img.i, ['img', browserSync.reload])
-        gulp.watch(path.data.i, ['data', browserSync.reload])
-        gulp.watch(path.include.i, ['include', browserSync.reload])
-    })
+	runSequence('clean:dist', 'default', function() {
+		browserSync.init({
+			server: "dist"
+		})
+		gulp.watch(path.js.i, ['js', browserSync.reload])
+		gulp.watch(path.css.i, ['sass', browserSync.reload])
+		gulp.watch(path.html.i, ['html', browserSync.reload])
+		gulp.watch(path.img.i, ['img', browserSync.reload])
+		gulp.watch(path.data.i, ['data', browserSync.reload])
+		gulp.watch(path.include.i, ['include', browserSync.reload])
+	})
 })
 
 // Bundle everything up ready for dropping onto the server
 // Destroy comments, remove console.logging, minify
 gulp.task('production', function(callback) {
-    console.log('production build started')
-    sassOptions = {
-        errLogToConsole: false,
-        outputStyle: 'compressed'
-    }
-    envProd = true
-    runSequence('clean:dist', 'sass', 'html', 'js', 'img', 'data', 'include', () => {
-      console.log('production build finished')
-    })
+	console.log('production build started')
+	sassOptions = {
+		errLogToConsole: false,
+		outputStyle: 'compressed'
+	}
+	envProd = true
+	runSequence('clean:dist', 'sass', 'html', 'js', 'img', 'data', 'include', () => {
+		console.log('production build finished')
+	})
 })
 
 // Delete the distribution folder
 gulp.task('clean:dist', function() {
-    return gulp.src('./dist', {read: false})
-	  .pipe(clean())
+	return gulp.src('./dist', {
+			read: false
+		})
+		.pipe(clean())
 })
 
 // HTML files
 gulp.task('html', function() {
-    gulp.src([path.html.i])
-    // Perform minification tasks, etc here
-	  .pipe(gulp.dest(path.html.o))
+	gulp.src([path.html.i])
+		// Perform minification tasks, etc here
+		.pipe(gulp.dest(path.html.o))
 })
 
 // Images
 gulp.task('img', function() {
-    gulp.src([path.img.i])
-    .pipe((envProd) ? imagemin({ progressive: true }) : noop())
-	  .pipe(gulp.dest(path.img.o))
+	gulp.src([path.img.i])
+		.pipe((envProd) ? imagemin({
+			progressive: true
+		}) : noop())
+		.pipe(gulp.dest(path.img.o))
 })
 
 // Data files
 gulp.task('data', function() {
-    gulp.src([path.data.i])
-    .pipe((envProd) ? jsonminify() : noop())
-	  .pipe(gulp.dest(path.data.o))
+	gulp.src([path.data.i])
+		.pipe((envProd) ? jsonminify() : noop())
+		.pipe(gulp.dest(path.data.o))
 })
 
 // 3rd party plugins
 gulp.task('include', function() {
-    gulp.src([path.include.i])
-	  .pipe(gulp.dest(path.include.o))
+	gulp.src([path.include.i])
+		.pipe(gulp.dest(path.include.o))
 })
 
 // Scss
 gulp.task('sass', function() {
-    return gulp.src(path.css.i)
-  	.pipe(sourcemaps.init())
-  	.pipe(sass(sassOptions).on('error', sass.logError))
-  	.pipe(sourcemaps.write())
-  	.pipe(autoprefixer((envProd) ? autoprefixerOptions : noop()))
-  	.pipe(gulp.dest(path.css.o))
+	return gulp.src(path.css.i)
+		.pipe(sourcemaps.init())
+		.pipe(sass(sassOptions).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(autoprefixer((envProd) ? autoprefixerOptions : noop()))
+		.pipe(gulp.dest(path.css.o))
 })
 
 // Javascript
 gulp.task('js', function() {
-    gulp.src(path.js.i)
-    	.pipe(sourcemaps.init())
-    	.pipe(concat('app.js'))
-    	.pipe(babel({presets: ['es2015'], minified: envProd}))
-      .pipe((envProd) ? stripDebug() : noop())
-      .pipe((envProd) ? strip() : noop())
-    	.pipe(sourcemaps.write('.'))
-    	.pipe(gulp.dest(path.js.o))
+	gulp.src(path.js.i)
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.js'))
+		.pipe(babel({
+			presets: ['es2015'],
+			minified: envProd
+		}))
+		.pipe((envProd) ? stripDebug() : noop())
+		.pipe((envProd) ? strip() : noop())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.js.o))
 })
